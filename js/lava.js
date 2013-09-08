33a910e029de4cfc86002b003326ed1504375066
Cat Johnson <cfjohnson@earthlink.net>
2013-09-07 18:14:06 -0600

    created menu, css, and initial javascript for lava menu

--- js/lava.js
+++ js/lava.js
@@ -1,3 +1,20 @@
 $(document).ready(function(){
 	console.log("here");
+
+	var style = 'easeOutCubic';
+	var default_left = Math.round($('#lava li.selected').offset().left - $('#lava').offset().left);
+	var default_width = $('#lava li.selected').width();
+
+	$('#box').css({left: default_left});
+	$('#box .head').css({width: default_width});
+
+	$('#lava li').hover(function(){
+
+		left = Math.round($(this).offset().left - $('#lava').offset().left);
+		width = $(this).width();
+
+		$('#box').stop(false, true).animate({left: left}, {duration:1000, easing: style});
+		$('#box .head').stop(false, true).animate({width:width}, {duration: 1000, easing: style});
+
+	})
 });
